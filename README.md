135be3878ae28cef5b625b0cf6791e91eabfa79d
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2025-08-20 14:36:46 +0200

    aggiunte alcune EPG manuali
    per Italia Channel, Padre Pio TV, Fascino TV 157, Radio Roma, TCI, RTR 99 e Radio Radio

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-Questa repo contiene una GitHub Action che genera l'EPG per Zappr ogni 4 ore. Il codice del "grabber" è in [/generate.mjs](/generate.mjs).
+Questa repo contiene una GitHub Action che genera l'EPG per Zappr ogni 4 ore. I vari grabber per le sorgenti sono nella cartella [`/sources`](/sources/).
 
 ## Sorgenti usate
 - **[blue TV](https://tv.blue.ch)**
-    - Per Mediaset, LA7, Sky, WBD, TV2000, RTL 102.5, Super!, Sportitalia, BIKE, SuperTennis, Alma TV, Deejay TV, Canale Italia, Digi 24, Parole di Vita, San Marino RTV, Class TV Moda, France 24 (in francese e in inglese) e Sky News
+    - Per Mediaset, LA7, Sky, WBD, TV2000, RTL 102.5, Super!, Sportitalia, SoloCalcio, BIKE, SuperTennis, Alma TV, Deejay TV, Canale Italia, Digi 24, Parole di Vita, San Marino RTV, Class TV Moda, France 24 (in francese e in inglese) e Sky News
 - **[RaiPlay](https://raiplay.it)**
     - Per i canali TV Rai
 - **[RaiPlay Sound](https://raiplaysound.it)**
     - Per i canali radio Rai
 - **[Tivù la Guida](https://tivu.tv)**
-    - Per QVC, SoloCalcio, Radio Italia
+    - Per QVC, Radio Italia e RDS Social TV
 - **[Mediaset Infinity](https://mediasetinfinity.mediaset.it)**
     - Per i canali TV di RadioMediaset
 - **[Cusano Media Play](https://cusanomediaplay.it)**
@@ -16,4 +16,10 @@ Questa repo contiene una GitHub Action che genera l'EPG per Zappr ogni 4 ore. Il
 - **[Publirose](https://publirose.it)**
     - Per Telecampione
 - **Sky**
-    - Per Gambero Rosso e Class CNBC
+    - Per EQUtv, Gambero Rosso e Class CNBC
+- **Samsung TV Plus**
+    - Per Urania e Italian Fishing TV
+- **RTL 102.5**
+    - Per Radiofreccia e Radio Zeta
+- **[Liste manuali](/sources/set/)**
+    - Per Italia Channel, Padre Pio TV, Fascino TV 157, Radio Roma, TCI, RTR 99 e Radio Radio
--- lists/it/dtt/national.js
+++ lists/it/dtt/national.js
@@ -9,17 +9,19 @@ import cusano from "../../../sources/cusano";
 import superguidatv from "../../../sources/superguidatv";
 import samsungtvplus from "../../../sources/samsungtvplus";
 import rtl from "../../../sources/rtl";
+import set from "../../../sources/set";
 
 export default {
     raiplay: { fetch: raiplay, channels: ["rai-1", "rai-2", "rai-3", "rai-4", "rai-5", "rai-movie", "rai-premium", "rai-gulp", "rai-yoyo", "rai-news-24", "rai-storia", "rai-scuola", "rai-sport", "rai-radio-2"] },
-    blue: { fetch: blue, channels: [348, 79, 215, 237, 266, 118, 1379, 214, 96, 1948, 633, 239, 243, 346, 1348, 1665, 1346, 1489, 2015, 191, 613, 51, 225, 189, 84, 412, 216, 393, 1287, 137, 257, 1646, 1384, 1387, 2054, 1386, 16, 1641, 80, 1650, 1951, 1719, 101, 183, 1639, 390] },
+    blue: { fetch: blue, channels: [348, 79, 215, 237, 266, 118, 1379, 214, 96, 1948, 633, 239, 243, 346, 1348, 1665, 1346, 1489, 2015, 191, 613, 51, 225, 189, 84, 412, 216, 393, 1287, 137, 257, 1646, 1384, 1387, 2049, 2054, 1386, 16, 1641, 80, 1650, 1951, 1719, 101, 183, 1639, 390] },
     mediaset: { fetch: mediaset, channels: ["ER", "EC", "EW"] },
     raiplaysound: { fetch: raiplaysound, channels: ["rai-radio-1", "rai-radio-2", "rai-radio-3", "rai-isoradio", "rai-radio-3-classica", "rai-radio-kids", "rai-radio-live-napoli", "rai-radio-techete", "rai-radio-tutta-italiana", "rai-radio-1-sport", "no-name-radio"] },
-    tivu: { fetch: tivu, channels: [130, 295, 136, 277] },
-    sky: { fetch: sky, channels: [9099, 308, 8293, 10616] },
+    tivu: { fetch: tivu, channels: [130, 136, 255] },
+    sky: { fetch: sky, channels: [9099, 308, 8293] },
     publirose: { fetch: publirose, channels: ["telecampione"] },
     cusano: { fetch: cusano, channels: ["canale122", "radiocusanocampus"] },
     superguidatv: { fetch: superguidatv, channels: [1284191387, 464] },
     samsungtvplus: { fetch: samsungtvplus, channels: ["IT600001L6", "IT50001295"] },
-    rtl: { fetch: rtl, channels: ["radiofreccia", "radiozeta"] }
+    rtl: { fetch: rtl, channels: ["radiofreccia", "radiozeta"] },
+    set: { fetch: set, channels: ["italiachannel", "padrepiotv", "fascinotv", "radioroma", "tci", "rtr99", "radioradio"] }
 };
