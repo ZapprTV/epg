8690e8a4f6cd455d6f8fe547a8b5bd1f3915509a
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2025-10-05 02:39:50 +0200

    disattivato temporaneamente SelfTV
    LENTO COME LA FAME

--- lists/it/dtt/regional/er.js
+++ lists/it/dtt/regional/er.js
@@ -9,7 +9,7 @@ import publirose from "../../../../sources/publirose";
 export default {
     ètv: { fetch: ètv, channels: ["Palinsesto_Marche"] },
     softwarecreation: { fetch: softwarecreation, channels: ["GoldTv", "LazioTv"] },
-    selftv: { fetch: selftv, channels: ["trmedia/eventsmo", "trmedia/eventsbo"] },
+    // selftv: { fetch: selftv, channels: ["trmedia/eventsmo", "trmedia/eventsbo"] },
     digitalbitrate: { fetch: digitalbitrate, channels: ["tca/TCA_CH32_9818", "tlu/TLU_CH34_25"] },
     oggiintv: { fetch: oggiintv, channels: ["Telepace"] },
     tv33: { fetch: tv33, channels: ["alto-adige"] },
