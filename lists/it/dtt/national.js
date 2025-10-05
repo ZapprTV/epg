9af156743b861c3387cb05da02da4433bd1c9acd
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2025-10-05 02:33:19 +0200

    rimpiazzato Warner TV con Discovery

--- lists/it/dtt/national.js
+++ lists/it/dtt/national.js
@@ -15,7 +15,7 @@ import plutotv from "../../../sources/plutotv";
 
 export default {
     raiplay: { fetch: raiplay, channels: ["rai-1", "rai-2", "rai-3", "rai-4", "rai-5", "rai-movie", "rai-premium", "rai-gulp", "rai-yoyo", "rai-news-24", "rai-storia", "rai-scuola", "rai-sport", "rai-radio-2"] },
-    blue: { fetch: blue, channels: [348, 79, 215, 237, 266, 118, 1379, 214, 96, 1948, 633, 239, 243, 346, 1348, 1665, 1346, 1489, 2015, 191, 613, 51, 225, 189, 84, 412, 216, 393, 1287, 137, 257, 1646, 1384, 1387, 2049, 2054, 1386, 16, 1641, 80, 2066, 1951, 1719, 101, 183, 1639, 390] },
+    blue: { fetch: blue, channels: [348, 79, 215, 237, 266, 118, 1379, 214, 96, 1948, 633, 239, 243, 346, 1348, 1665, 1346, 1489, 124, 191, 613, 51, 225, 189, 84, 412, 216, 393, 1287, 137, 257, 1646, 1384, 1387, 2049, 2054, 1386, 16, 1641, 80, 2066, 1951, 1719, 101, 183, 1639, 390] },
     mediaset: { fetch: mediaset, channels: ["ER", "EC", "EW"] },
     raiplaysound: { fetch: raiplaysound, channels: ["rai-radio-1", "rai-radio-2", "rai-radio-3", "rai-isoradio", "rai-radio-3-classica", "rai-radio-kids", "rai-radio-live-napoli", "rai-radio-techete", "rai-radio-tutta-italiana", "rai-radio-1-sport", "no-name-radio"] },
     tivu: { fetch: tivu, channels: [130, 136, 255] },
