e71619d6283a7e38f08cf8400bedaa7ca8fa4949
Francesco <52544979+FrancescoRosi@users.noreply.github.com>
2025-10-14 22:06:55 +0200

    aggiunta l'EPG a Telelombardia e Antenna 3 Lombardia

--- lists/it/dtt/regional/lombardia.js
+++ lists/it/dtt/regional/lombardia.js
@@ -7,7 +7,7 @@ import cr1 from "../../../../sources/it/cr1";
 import publirose from "../../../../sources/it/publirose";
 
 export default {
-    blue: { fetch: blue, channels: [1667, 1645, 593, 2113] },
+    blue: { fetch: blue, channels: [592, 1380, 1667, 1645, 593, 2113] },
     digitalbitrate: { fetch: digitalbitrate, channels: ["tlu/TLU_CH22_2003", "tlu/TLU_CH22_2005", "tlu/TLU_CH34_25", "tlu/TLU_CH34_31", "tlu/TLU_CH34_30", "tlu/TLU_CH22_2014"] },
     tv33: { fetch: tv33, channels: ["alto-adige"] },
     ètv: { fetch: ètv, channels: ["Palinsesto_Marche"] },
